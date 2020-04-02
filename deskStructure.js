43a21d2c130e91f01fd040b6fefc731c2f3a89d3
Kate <katya@breadhead.ru>
2020-04-02 10:40:28 +0700

    linter check and fix

--- deskStructure.js
+++ deskStructure.js
@@ -5,27 +5,27 @@ export default () =>
     .id('main')
     .items([
       S.listItem()
-          .id('partners')
-          //.icon()
-          .title('Партнеры')
-          .child(
-              S.documentList()
-                  .id('allPartners')
-                  .title('Партнеры')
-                  .menuItems(S.documentTypeList('partner').getMenuItems())
-                  .filter('_type == $type')
-                  .params({ type: 'partner' }),
-          ),
-        S.listItem()
-            .id('experts')
-            //.icon()
+        .id('partners')
+        //.icon()
+        .title('Партнеры')
+        .child(
+          S.documentList()
+            .id('allPartners')
+            .title('Партнеры')
+            .menuItems(S.documentTypeList('partner').getMenuItems())
+            .filter('_type == $type')
+            .params({ type: 'partner' }),
+        ),
+      S.listItem()
+        .id('experts')
+        //.icon()
+        .title('Эксперты')
+        .child(
+          S.documentList()
+            .id('allExperts')
             .title('Эксперты')
-            .child(
-                S.documentList()
-                    .id('allExperts')
-                    .title('Эксперты')
-                    .menuItems(S.documentTypeList('expert').getMenuItems())
-                    .filter('_type == $type')
-                    .params({ type: 'expert' }),
-            )
+            .menuItems(S.documentTypeList('expert').getMenuItems())
+            .filter('_type == $type')
+            .params({ type: 'expert' }),
+        ),
     ]);
